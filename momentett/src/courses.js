89895a9d0094e31d7201521c7eb82f3c9a3b5e37
Tornrosen <elec11anna@hotmail.com>
2025-04-01 14:08:27 +0200

    använda tsc

--- momentett/src/courses.js
+++ momentett/src/courses.js
@@ -1,13 +1,54 @@
-var course = {
-    code: "DT092G",
-    name: "Webbutveckling",
-    progression: "A",
-    syllabus: "URL"
-};
-function printUser(course) {
-    console.log(course.code);
-    console.log(course.name);
-    console.log(course.progression);
-    console.log(course.syllabus);
+window.onload = init;
+var inputCodeEl = document.getElementById("courseCode");
+var inputNameEl = document.getElementById("courseName");
+var inputProgEl = document.getElementById("courseProg");
+var inputSyllEl = document.getElementById("syllabus");
+var courseListEl = document.getElementById("courseList");
+var errorSpace1El = document.getElementById("errorSpace1");
+var errorSpace2El = document.getElementById("errorSpace2");
+var addBtnEl = document.getElementById("addBtn");
+addBtnEl.addEventListener("click", addCourses);
+inputProgEl.addEventListener("keyup", checkInput);
+function addCourses() {
+    var course = {
+        code: inputCodeEl.value,
+        name: inputNameEl.value,
+        progression: inputProgEl.value,
+        syllabus: inputSyllEl.value
+    };
+    if (courseListEl) {
+        courseListEl.textContent = "Kurskod: ".concat(course.code, ". Kursnamn: ").concat(course.name, ". \n  Progression: ").concat(course.progression, ". Kursplan: ").concat(course.syllabus, ".");
+        inputCodeEl.value = "";
+        inputNameEl.value = "";
+        inputProgEl.value = "";
+        inputSyllEl.value = "";
+    }
+    saveCourses(course);
 }
-printUser(course);
+function checkInput() {
+    var input = inputProgEl.value;
+    if (input === "A" || input === "B" || input === "C") {
+        addBtnEl.disabled = false;
+        errorSpace1El.innerHTML = "";
+    }
+    else {
+        addBtnEl.disabled = true;
+        errorSpace1El.innerHTML = "Välj A, B eller C!";
+    }
+}
+function saveCourses(course) {
+    var jsonStr = JSON.stringify(course);
+    localStorage.setItem("course", jsonStr);
+}
+function init() {
+    loadCourses();
+}
+function loadCourses() {
+    if (localStorage != null) {
+        var savedCourse = JSON.parse(localStorage.getItem("course"));
+        if (courseListEl) {
+            courseListEl.textContent = "Kurskod: ".concat(savedCourse.code, ". Kursnamn: ").concat(savedCourse.name, ". \n      Progression: ").concat(savedCourse.progression, ". Kursplan: ").concat(savedCourse.syllabus, ".");
+        }
+    }
+    ;
+}
